df6bff97988c45b463ce54e544c3432cad03be34
Fidel Esteban Morales Cifuentes <fidmor.89@outlook.com>
2016-01-18 11:25:53 -0600

    Initial Commit

---
+++ VSTSV2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  VSTSV2
+//
+//  Created by Fidel Esteban Morales Cifuentes on 1/18/16.
+//  Copyright (c) 2016 Fidel Esteban Morales Cifuentes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ VSTSV2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  VSTSV2
+//
+//  Created by Fidel Esteban Morales Cifuentes on 1/18/16.
+//  Copyright (c) 2016 Fidel Esteban Morales Cifuentes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
